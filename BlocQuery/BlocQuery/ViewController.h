266d9ad7eb06aaa9777c1a9d8c236d93a05fa62c
Melissa <melbrng@gmail.com>
2015-12-06 14:31:16 -0700

    initial commit of BlocQuery xcode project

---
+++ BlocQuery/BlocQuery/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BlocQuery
+//
+//  Created by Melissa Boring on 12/6/15.
+//  Copyright © 2015 melbo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BlocQuery/BlocQuery/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  BlocQuery
+//
+//  Created by Melissa Boring on 12/6/15.
+//  Copyright © 2015 melbo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
